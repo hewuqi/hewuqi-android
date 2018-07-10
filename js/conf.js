ee613026d0f7c7fcd3161a9a022d0805cbb3e622
yaphone <zhouyaphone@163.com>
2018-07-10 23:50:27 +0800

    日常开发
    日常开发

---
+++ js/conf.js
@@ -0,0 +1,6 @@
+const SERVER_URL = "https://localhost"
+const qiniuConfig = {
+	useCdnDomain: true
+}
+const serverUrl = "https://115.29.33.119"
+//const serverUrl = "http://127.0.0.1"
---
+++ page/home/home.js
@@ -0,0 +1,11 @@
+var homeApp = new Vue({
+	el: '#homeApp',
+	data: {
+		menu: [
+			compare: {
+				icon: "../../image/menu/compare.png",
+				title: "人脸比对"
+			}
+		]
+	}
+})
