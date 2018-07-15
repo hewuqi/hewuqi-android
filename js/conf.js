62c6465a2661c6ff051770844c11069bd27c3057
yaphone <zhouyaphone@163.com>
2018-07-15 13:44:34 +0800

    日常开发
    日常开发

--- js/conf.js
+++ js/conf.js
@@ -2,5 +2,5 @@ const SERVER_URL = "https://localhost"
 const qiniuConfig = {
 	useCdnDomain: true
 }
-const serverUrl = "https://115.29.33.119"
-//const serverUrl = "http://127.0.0.1"
+//const serverUrl = "https://115.29.33.119"
+const serverUrl = "http://127.0.0.1"
